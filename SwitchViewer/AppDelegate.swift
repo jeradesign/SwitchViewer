f63c45a84ecb680b2cd118af82ce13add222a186
John Brewer <jbrewer@jera.com>
2022-08-26 22:28:19 -0700

    Initial Commit

---
+++ SwitchViewer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SwitchViewer
+//
+//  Created by John Brewer on 8/26/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SwitchViewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwitchViewer
+//
+//  Created by John Brewer on 8/26/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
